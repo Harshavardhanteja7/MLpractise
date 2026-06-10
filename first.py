fdd6570365bf1024d0c67f7bca6a9f37239e124e
Harshavardhan Teja <harshavardhanteja@Harshavardhans-MacBook-Air.local>
2026-06-10 11:25:48 +0530

    New chnages

---
+++ first.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+
+overs = [1,2,3,4,5,6]
+runs = [10,22,15,9,20,15]
+
+
+x = np.array(overs).reshape(-1,1)
+y = np.array(runs)
+
+model = linear_model.LinearRegression()
+model.fit(x, y)
+
+pred_over = 3
+pred_runs = model.predict([[pred_over]])
+
+print(f"{pred_runs[0]:.2f}")
